ae3652ecc950f95b5cab604ae85d53dd8e49df41
LuapEmaj <jamepauljr@gmail.com>
2026-03-13 20:38:17 +0800

    added code in temperature.php to convert Celsius to Fahrenheit and vice versa.

--- temperature.php
+++ temperature.php
@@ -2,4 +2,20 @@
 
 <html>
     <head>
-        
+        <title>Celsius to Farenheit Conversion Table</title>
+    </head> 
+    <body>
+        <h1>Celsius to Farenheit Conversion Table</h1>
+        <table border="1">
+            <tr>
+                <th>Celsius</th>
+                <th>Farenheit</th>
+            </tr>
+            <?php
+                for ($celsius = 0; $celsius <= 100; $celsius += 10) {
+                    $farenheit = ($celsius * 9 / 5) + 32;
+                    echo "<tr><td>$celsius</td><td>$farenheit</td></tr>";
+                }
+            ?>
+        </table>
+    </body>
